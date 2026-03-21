bad97dec960512ba9fe7cb532c13f385e007c42c
Romanov Vyacheslav <vyacheslavdroid@gmail.com>
2026-03-21 11:42:00 +0300

    feat(design-system): добавил Color.kt — токены White, Beige, Secondary, DarkBrown

---
+++ core/design-system/src/main/java/com/coffeeshop/design_system/Color.kt
@@ -0,0 +1,8 @@
+package com.coffeeshop.design_system
+
+import androidx.compose.ui.graphics.Color
+
+val White = Color(0xFFFFFFFF)
+val Beige = Color(0xFFF6EBE2)
+val Secondary = Color(0xFF928484)
+val DarkBrown = Color(0xFF453E3E)
