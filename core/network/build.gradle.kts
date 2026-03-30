1aab04d280271bdac9963fea3ac08a155eae02fa
Romanov Vyacheslav <vyacheslavdroid@gmail.com>
2026-03-30 20:24:27 +0300

    feat(project): настроил зависимости

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -2,6 +2,8 @@ import org.gradle.kotlin.dsl.implementation
 
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
@@ -38,9 +40,17 @@ android {
 dependencies {
     implementation(rootProject.files("libs/api-contracts.jar"))
 
+    implementation(libs.room)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+    implementation(libs.coroutines)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.retrofit.kotlinx.serialization.converter)
+    implementation(libs.retrofit)
+    implementation(libs.okhttp.logging)
+    implementation(libs.kotlin.reflect)
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
