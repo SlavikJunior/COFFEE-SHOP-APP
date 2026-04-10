29cda7db294384dd4bfc1dc178167cc0769f6cea
Romanov Vyacheslav <vyacheslavdroid@gmail.com>
2026-04-10 15:11:46 +0300

    refactor(navigation): заменить FeatureEntryBuilder/Navigator на Route-интерфейс

--- core/navigation/src/main/kotlin/com/coffeshop/navigation/FeatureEntryBuilder.kt
+++
@@ -1,8 +0,0 @@
-package com.coffeshop.navigation
-
-import androidx.navigation3.runtime.EntryProviderScope
-import androidx.navigation3.runtime.NavKey
-
-interface FeatureEntryBuilder<T : NavKey> {
-    fun build(scope: EntryProviderScope<T>)
-}
---
+++ core/navigation/src/main/kotlin/com/coffeshop/navigation/Route.kt
@@ -0,0 +1,7 @@
+package com.coffeshop.navigation
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+interface Route : NavKey
